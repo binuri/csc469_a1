3d56268fa199d1558b4fe386eb37400afb78d4da
Walpitagamage Binuri Mandula <g3tdirty@cdf.toronto.edu>
2016-02-10 16:42:20 -0500

    Plotting process activity in a single graph for part A2

--- context_tracker.c
+++ context_tracker.c
@@ -20,11 +20,7 @@ void print_sample_results(int process_num, int num_samples, u_int64_t start_time
     
     int i = 0;
     while (i < num_samples){
-        if (verbose) {
-            printf ("\n__print_results start_time: %"PRIu64"\n", start_time);
-            printf ("__print_results %d: %"PRIu64" to %"PRIu64"\n",
-                        i, samples[i * 2], samples[2 * i + 1]);
-        }
+        
         printf("[Child %d]Active %d: start at %"PRIu64
                         " duration %"PRIu64" cycles (%f ms)\n",
             process_num,
@@ -56,11 +52,11 @@ int main(int argc, char *argv[])
 {
     if(argc < 2) {
         printf ("ERROR: Invalid number of arguments. Please use the following format:\n");
-        printf ("tracker_context <num_samples>\n");
+        printf ("context_tracker <num_samples> <num_child_procs>\n");
         return EXIT_FAILURE;
     }
 
-        char *err;
+    char *err;
     int num_samples = strtoll(argv[1], &err, 10);
     if (*err) {
         printf ("ERROR: Invalid integer value given for 'num_samples'\n");
@@ -68,8 +64,10 @@ int main(int argc, char *argv[])
     }    
 
     // Check if verbose flag is found
-    if (argc == 3 && strcmp(argv[2], "-v") == 0) {
-        verbose = 1;
+    int num_child_procs = strtoll(argv[2], &err, 10);
+    if (*err) {
+        printf ("ERROR: Invalid integer value given for 'num_child_procs'\n");
+        return EXIT_FAILURE;
     }
     
     // Pinning the process to 0th CPU
@@ -85,7 +83,7 @@ int main(int argc, char *argv[])
     u_int64_t clock_speed = get_estimated_clock_speed();
 
     start_counter();
-    for (int i = 0; i < 5 ; i++) {
+    for (int i = 0; i < num_child_procs ; i++) {
         pid_t pid = fork();
         if (pid == -1) {
             perror("Error when forking");
@@ -105,7 +103,7 @@ int main(int argc, char *argv[])
     }
 
     
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < num_child_procs; i++) {
         int status;
         pid_t pid = wait(&status);
         if (pid == -1) {
